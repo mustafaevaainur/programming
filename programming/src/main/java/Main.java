e73ae926366b728ecc8bbe7966212fbfcdedb8f7
Ainur <79215797496@yandex.ru>
2018-02-13 22:25:27 +0300

    first commit

---
+++ programming/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] n = {3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25}; //govnocode k
+        double[] x = new double[0];
+    }
+}
