09df7879b9e4ef2d7a264a867cf537c7a75ca6fd
Ainur <79215797496@yandex.ru>
2018-02-15 15:58:48 +0300

    add Main.java methods

--- programming/src/main/java/Main.java
+++ programming/src/main/java/Main.java
@@ -1,26 +1,63 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
 
-        int iSize = 12;
-        int jSize = 15;
+    public static void main(String[] args) throws Exception{
+        System.out.println("* * * Magic! * * *");
+        Main wwD = new Main();
+
+
+        //nArray()
+        int count=0;
+        int lengh=0;
+        int[] n = new int[lengh];
+        int index;
+        for (count=3; count<=25; count++) {
+            if (count%2!=0) lengh++;
+        }
+        System.out.println(lengh);
+        for (index=1, count=0; index<=count; index++) {
+            if (index % 2 != 0) {
+                n[count]=index;
+                count++;
+                System.out.println(n[count]);
+            }
+        }
+    }
+ // заполнить массив нечетными числами
+    /*public static int nArray(int index) {
+        int[] n = new int[26];
+        for (index=1; index<=25; index++) {
+            n[index]=index*2+1;
+            System.out.println(n[index]);
+        }
+        return n[index];
+    }
+*/
+    public static double xArray(int count) {
         int xSize = 15;
-
-        int[] n = {3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25}; //govnocode
         double[] x = new double[xSize];
-        double[][] u = new double[iSize][jSize];
-
+        double xtemp = 0.0;
         double minVal = -6.0;
         double maxVal = 8.0;
-        for (int t=0; t<x.length; t++) {
-            x[t] = (minVal + (Math.random()* maxVal));
-            System.out.println(t + ": " + String.valueOf(x[t]));
+        for (count = 0; count < x.length; count++) {
+            x[count] = (minVal + (Math.random() * maxVal));
+            xtemp = x[count];
         }
+        return xtemp;
+    }
 
-        for (int i=0; i<iSize; i++){
-            for (int j=0; j<jSize; j++) {
-                System.out.println();
+    private double x;
+
+    public void workWithDoubleArray() {
+        int iSize = 12;
+        int jSize = 15;
+        double[][] u = new double[iSize][jSize];
+        for (int i = 0; i < iSize; i++) {
+            for (int j = 0; j < jSize; j++) {
+                this.x = xArray(j);
+
             }
         }
     }
